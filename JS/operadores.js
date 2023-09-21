07c092138a1804e4fd82915bec12603cd1f71720
Juan David Bedoya <juan.bedoyah@cun.edu.co>
2023-09-21 17:30:33 -0500

    actualizacion 21 sep

---
+++ JS/operadores.js
@@ -0,0 +1,70 @@
+//definición de variables
+let num3,num4, resultado
+
+//asignacion de valores a variables
+//num1 = 5
+//num2 = 15
+/*
+//suma
+resultado= num1 + num2
+//imprimir resultado
+console.log(`Es resultado de la suma es: ${resultado}`)
+
+//resta
+resultado = num1 - num2
+console.log(`El resultado de la resta es: ${resultado}`)
+
+//multiplicación
+resultado = num1 * num2
+console.log(`El resultado de la multiplicación es: ${resultado}`)
+
+//división
+resultado = num1 / num2
+console.log(`El resultado de la division es ${resultado}`)
+
+//Modulo
+resultado = num1 % num2
+console.log(`El resultado de el modulo es: ${resultado}`)
+
+//Exponenciación
+resultado = num1 ** num2
+console.log(`El resultado de la exponenciación es: ${resultado}`)
+*/
+//let num3, num4
+/*num3 = 9
+num4 = 6
+
+resultado = num3 + num4
+alert(`la suma entre ${num3} y ${num4} es ${resultado}`)
+*/
+num3 = prompt('Digite el primer número')
+num4 = prompt('Digite el segundo número')
+resultado =  parseInt (num3) + parseInt (num4)
+console.log(`la suma entre ${num3} y ${num4} es ${resultado}`)
+alert(`la suma entre ${num3} y ${num4} es ${resultado}`)
+
+//resta
+
+num3 = prompt('Digite el primer número')
+num4 = prompt('Digite el segundo número')
+resultado = parseInt(num3) - parseInt(num4)
+console.log(`la resta entre ${num3} y ${num4} es ${resultado}`)
+alert(`La resta entre ${num3} y ${num4} es ${resultado}`)
+
+//Multiplicacion
+
+num3 = prompt('Digite el primer número')
+num4 = prompt('Digite el segundo número')
+resultado = parseInt(num3) * parseInt(num4)
+console.log(`El producto entre ${num3} y ${num4} es ${resultado}`)
+alert(`El producto ${num3} y ${num4} es ${resultado}`)
+
+//División
+
+num3 = prompt('Digite el primer número')
+num4 = prompt('Digite el segundo número')
+resultado = parseInt(num3) / parseInt(num4)
+console.log(`El producto entre ${num3} y ${num4} es ${resultado}`)
+alert(`El producto ${num3} y ${num4} es ${resultado}`)
+
+//
