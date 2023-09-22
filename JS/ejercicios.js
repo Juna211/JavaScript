3fe7713af6b14031de7c632b622bead390cff8f4
Juan David Bedoya <juan.bedoyah@cun.edu.co>
2023-09-22 09:20:47 -0500

    actualizacion 22 sep

---
+++ JS/ejercicios.js
@@ -0,0 +1,38 @@
+//variables
+/*
+num1 = prompt('Digite un numero')
+alert(`El número digitado es ${num1}`)
+console.log(`El número digitado es ${num1}`)
+
+//ejercicio 2}
+
+num2 = prompt ('Digite un numero')
+num2 = parseInt(num2) * 2
+alert(`El número digitado es ${num2}`)
+console.log(`El número multiplicado por 2 es ${num2}`)
+*/
+let num1 , num2 , num3 , num4 , num5, num6, resultado
+//ejercicio 3
+/*
+num3 = prompt ('Digite un numero')
+num3 = parseInt(num3) * parseInt(num3)
+alert(`El numero digitado es ${num3}`)
+console.log(`El número al cuadrado es ${num3}`)
+*/
+
+/*num4 = prompt ('Digite un numero')
+num4 = parseInt (num4) * parseInt(num4) * parseInt(num4)
+alert(`El numero digitado es ${num4}`)
+console.log(`El numero alcubo es ${num4}`)
+*/
+
+num5 =prompt ('Digite un numero')
+num6 = prompt ('Digite un numero')
+resultado = parseInt (num5) + parseInt(num6)
+alert(`El numero digitado es ${resultado}`)
+console.log(`la suma entre 2 numeros es ${resultado}`)
+
+
+
+
+
